97e6e35487f487c5c321e1ef2368b04cbee2a10d
DevnishMishra <devnishmishra@gmail.com>
2026-03-08 16:31:47 +0530

    Connect MongoDB using Mongoose

--- index.js
+++ index.js
@@ -9,3 +9,13 @@ app.use(express.json());
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+import mongoose from "mongoose";
+
+mongoose
+  .connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("MongoDB connected"))
+  .catch((err) => console.error("MongoDB connection error:", err));
