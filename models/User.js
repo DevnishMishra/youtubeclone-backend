4b39168927d1a69aeccb04185246880355420eb2
DevnishMishra <devnishmishra@gmail.com>
2026-03-08 16:32:41 +0530

    Create User model

---
+++ models/User.js
@@ -0,0 +1,14 @@
+import mongoose from "mongoose";
+
+const UserSchema = new mongoose.Schema(
+  {
+    username: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    avatar: { type: String, default: "" },
+    channels: [{ type: mongoose.Schema.Types.ObjectId, ref: "Channel" }],
+  },
+  { timestamps: true },
+);
+
+export default mongoose.model("User", UserSchema);
